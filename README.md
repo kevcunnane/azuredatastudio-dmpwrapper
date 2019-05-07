6d89258c62343e89d15fb7c836912a3f6df3be67
Kevin Cunnane <kcunnane@microsoft.com>
2019-05-07 13:23:18 -0700

    Create module with wrapper for connect and query

---
+++ README.md
@@ -0,0 +1,30 @@
+# AzureDataStudio DMP Wrapper
+
+A wrapper to simplify using Data Management Protocol APIs in Azure Data Studio.
+
+## Usage
+```ts
+// Create a Connection context to simplify connecting via the Data Management Protocol
+let connectionProvider = sqlops.dataprotocol.getProvider<sqlops.ConnectionProvider>('MSSQL', sqlops.DataProviderType.ConnectionProvider);
+let connectionContext = new ConnectionContext(connectionProvider);
+
+// Connect once
+let connected = await connectionContext.tryConnect(connection);
+if (!connected) {
+    return;
+}
+
+// Run queries
+let result: sqlops.SimpleExecuteResult = await connectionContext.runQueryAndReturn('select 1 as MyColumn');
+if (result.columnInfo[0].columnName !== 'MyColumn') {
+    throw new Error('Query did not work');
+} else {
+    vscode.window.showInformationMessage(`Value returned from query was ${result.rows[0][0].displayValue}`);
+}
+
+// Close connection
+connectionContext.dispose();
+```
+
+## Licensing
+This is free to use under the MIT license
---
+++ src/index.ts
@@ -0,0 +1,90 @@
+//
+// Copyright (c) Kevin Cunnane. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+//
+
+'use strict';
+
+import * as sqlops from 'sqlops';
+import { Deferred } from './promise';
+
+let counter = 0;
+let connectionTracker = new Map<string, Deferred<void>>();
+let connectionProviders = new Set<string>();
+
+export interface IDisposable {
+    dispose(): void;
+}
+
+function onConnectionCompleteHandler(summary: sqlops.ConnectionInfoSummary): void {
+    let trackedPromise = connectionTracker.get(summary.ownerUri);
+    if (trackedPromise) {
+        if (summary.connectionId) {
+            // Having a connectionId indicates success (not sure why)
+            trackedPromise.resolve();
+        } else {
+            trackedPromise.reject(summary.errorMessage);
+        }
+        connectionTracker.delete(summary.ownerUri);
+    }
+}
+
+export class ConnectionContext implements IDisposable {
+
+    private connected: boolean = false;
+    private connectionUri: string;
+    constructor(private connectionProvider: sqlops.ConnectionProvider) {
+        if (this.connectionProvider && !connectionProviders.has(connectionProvider.providerId)) {
+            this.connectionProvider.registerOnConnectionComplete(onConnectionCompleteHandler);
+        }
+        this.connectionUri = `untitled:createdb${counter++}`;
+
+    }
+
+    public async tryConnect(connection: sqlops.IConnectionProfile | sqlops.connection.Connection): Promise<boolean> {
+        try {
+            let deferred = new Deferred<void>();
+            connectionTracker.set(this.connectionUri, deferred);
+
+            let connectRequested = await this.connectionProvider.connect(this.connectionUri, connection);
+            if (!connectRequested) {
+                return false;
+            }
+            // Wait on the connection to complete, or 15 seconds (mostly in case our logic is faulty)
+            await Promise.race([deferred.promise, this.errorOnTimeout(15000)]);
+        } catch (err) {
+            return false;
+        } finally {
+            // Always clean up the connection from the tracker
+            connectionTracker.delete(this.connectionUri);
+        }
+
+        this.connected = true;
+        return this.connected;
+    }
+
+    /**
+     * Simple execution of a query.
+     * The query must be a single resultset response 
+     * @param query 
+     */
+    public async runQueryAndReturn(query: string): Promise<sqlops.SimpleExecuteResult> {
+        if (!this.connected) {
+            throw new Error('Connection must be created before running queries');
+        }
+        let queryProvider = sqlops.dataprotocol.getProvider<sqlops.QueryProvider>(this.connectionProvider.providerId, sqlops.DataProviderType.QueryProvider);
+        let result = await queryProvider.runQueryAndReturn(this.connectionUri, query);
+        return result;
+    }
+
+    public dispose() {
+        if (this.connected) {
+            // Disconnect and ignore any errors since a failure means the connection wasn't really established.
+            this.connectionProvider.disconnect(this.connectionUri).then(success => undefined, fail => undefined);
+        }
+    }
+
+    private errorOnTimeout(ms: number): Promise<void> {
+        return new Promise((resolve, reject) => setTimeout(reject, ms));
+    }
+}
